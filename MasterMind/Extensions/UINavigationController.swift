8ee728618a1198517ff8ed9a157e89da95c954a9
zmercedes <zmercedes89@gmail.com>
2020-02-04 09:58:41 -0500

    feat: add UINavigationController extension
    This commit adds a UINavigationController extension setStyle() which sets the style for the navigation bar.

---
+++ MasterMind/Extensions/UINavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  UINavigationController.swift
+//  MasterMind
+//
+//  Created by Zoilo Mercedes on 2/4/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    func setStyle() {
+        navigationBar.isTranslucent = false
+        navigationBar.barTintColor = .black
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+    }
+}

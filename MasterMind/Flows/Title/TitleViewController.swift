5a6bc83e08f18332fc2d3a3f0bc76f1ff4bde0d1
zmercedes <zmercedes89@gmail.com>
2020-02-03 21:58:03 -0500

    feat: add TitleViewController
    This commit adds a TitleViewController and associated view.

---
+++ MasterMind/Flows/Title/TitleViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TitleViewController.swift
+//  MasterMind
+//
+//  Created by Zoilo Mercedes on 2/3/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func findButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func settingsButtonPressed(_ sender: Any) {
+        
+    }
+    
+}

cd3a6904d8236489fdf7034a3f131e26ee13e77f
zmercedes <zmercedes89@gmail.com>
2020-02-04 13:37:51 -0500

    feat: add end sequence to GameCoordinator
    This commit adds a delegate protocol to GameCoordinator with an ended() and an end destination which calls ended() and dismisses the game view controller. It now stores the navigationController in order to do this.

--- MasterMind/Flows/Game/GameCoordinator.swift
+++ MasterMind/Flows/Game/GameCoordinator.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
+protocol GameCoordinatorDelegate: class {
+    func ended()
+}
+
 class GameCoordinator: Coordinator {
     
     enum Destination {
         case nextLevel
+        case end
     }
     
+    private let navigationController: UINavigationController
     private let dependencies: Dependencies
     private var currentCombination: [Int]?
+    weak var delegate: GameCoordinatorDelegate?
     
-    init(dependencies: Dependencies){
+    init(navigation: UINavigationController, dependencies: Dependencies){
         self.dependencies = dependencies
+        self.navigationController = navigation
     }
     
     func start() {
@@ -28,12 +36,17 @@ class GameCoordinator: Coordinator {
         if let newDigits = RandomAPI.get(total: total, min: min, max: max).result() {
             currentCombination = newDigits
         }
+        let viewController = UIViewController()
+        navigationController.pushViewController(viewController, animated: false)
     }
     
     func navigate(to destination: Destination) {
         switch destination {
-        case .nextLevel :
+        case .nextLevel:
             print(" moved to next level!")
+        case .end:
+            navigationController.popViewController(animated: false)
+            delegate?.ended()
         }
     }
 }

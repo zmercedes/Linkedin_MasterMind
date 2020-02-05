781bc6f375736ca66a5b5568373e15a34ed734dc
zmercedes <zmercedes89@gmail.com>
2020-02-04 20:24:50 -0500

    refactor: RandomAPI
    This commit changes RandomAPI to return a string of digits instead of an array of integers.

--- MasterMind/Services/RandomAPI.swift
+++ MasterMind/Services/RandomAPI.swift
@@ -12,15 +12,14 @@ enum RandomAPI { }
 
 extension RandomAPI {
     struct get {
-        let result: (()-> [Int]?)
+        let result: (()-> String?)
         init(total: Int, min: Int, max: Int){
             let url = "https://www.random.org/integers/?num=\(total)&min=\(min)&max=\(max)&col=1&base=10&format=plain&rnd=new"
             result = {
                 do {
                     let data = try Data(contentsOf: URL(string: url)!)
-                    let numbers = String(data: data, encoding: .utf8)!.components(separatedBy: "\n")
-                        .compactMap { Int($0) }
-                    return numbers
+                    let result = String(data: data, encoding: .utf8)!.components(separatedBy: "\n").joined()
+                    return result
                 } catch {
                     return nil
                 }

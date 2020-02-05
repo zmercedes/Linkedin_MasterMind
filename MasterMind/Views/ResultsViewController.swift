ffee33376c0a9aeb6f7d833bd1232452ea612078
zmercedes <zmercedes89@gmail.com>
2020-02-05 17:56:33 -0500

    feat: add ResultsView
    This commit adds ResultsView, a custom alert which lets the user know the results of their game session when it ends, or if there is an error.

---
+++ MasterMind/Views/ResultsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ResultsViewController.swift
+//  MasterMind
+//
+//  Created by Zoilo Mercedes on 2/5/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: CustomAlertViewController {
+
+    @IBOutlet weak var alert: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var okAction: (() -> Void)?
+    var titleText: String
+    var bodyText: String
+    
+    init(title: String, body: String, action: (() -> Void)? = nil) {
+        self.titleText = title
+        self.bodyText = body
+        okAction = action
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = titleText
+        alert.layer.cornerRadius = 6
+        alert.layer.borderWidth = 1
+        alert.layer.borderColor = UIColor.white.cgColor
+        alertView = alert
+    }
+    
+    @IBAction func okButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true) {
+            self.okAction?()
+        }
+    }
+    
+}

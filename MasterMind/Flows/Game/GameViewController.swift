9928926d0937fbdb2a5746b994ff2c68b0533996
zmercedes <zmercedes89@gmail.com>
2020-02-05 19:29:56 -0500

    feat: finalized basic game loop
    This commit adds functionality which finalizes the game loop.

--- MasterMind/Flows/Game/GameViewController.swift
+++ MasterMind/Flows/Game/GameViewController.swift
@@ -27,11 +27,15 @@ class GameViewController: UIViewController {
     @IBOutlet weak var triesTableView: UITableView!
     @IBOutlet weak var triesTableViewHeight: NSLayoutConstraint!
     
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var quitButton: UIButton!
+    
     weak var delegate: GameViewControllerDelegate?
     
-    var currentCombination: String = ""
-    var guesses: [String] = []
-    var guessDict: [String:[Int]] = [:]
+    private var currentCombination: String = ""
+    private var guesses: [String] = []
+    private var guessDict: [String:[Int]] = [:]
+    private var victory: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,13 +45,9 @@ class GameViewController: UIViewController {
         resultsView.layer.borderColor = UIColor.white.cgColor
         if let newCombo = delegate?.requestDigits() {
             currentCombination = newCombo
+            print(currentCombination)
         } else {
-            let alertTitle = "Error"
-            let body = "Could not find any target. Try again later."
-            let alert = ResultsViewController(title: alertTitle, body: body) { [unowned self] in
-                self.delegate?.gameOver()
-            }
-            self.present(alert, animated: true, completion: nil)
+            error()
         }
         guessField.attributedPlaceholder = NSAttributedString(string: "guess here", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
         guessField.addTarget(self, action: #selector(onEditNextToggle(_:)), for: .editingChanged)
@@ -78,6 +78,14 @@ class GameViewController: UIViewController {
         triesTableView.isHidden = !hidden
     }
     
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        reset()
+    }
+    
+    @IBAction func quitButtonPressed(_ sender: Any) {
+        delegate?.gameOver()
+    }
+    
     private func displayResults() {
         if !resultsView.isHidden {
             resultsView.isHidden = true
@@ -94,6 +102,7 @@ class GameViewController: UIViewController {
             placeLabel.text = ""
         } else {
             let matches = currentCombination.match(other: guess)
+            victory = matches[0] == 4 && matches[1] == 4
             let resultText = matches[0] > 0 ? "\(matches[0]) digits matched!" : "No digits matched."
             let placeText = matches[1] > 0 ? "\(matches[1]) matched in right place!" : ""
             resultLabel.text = resultText
@@ -101,9 +110,62 @@ class GameViewController: UIViewController {
             guesses.append(guess)
             guessDict[guess] = matches
         }
+        if victory {
+            success()
+        } else {
+            if guesses.count == 10 {
+                failure()
+            }
+        }
         triesTableViewHeight.constant = guesses.count > 0 ? CGFloat(42*guesses.count) : 42
         triesTableView.reloadData()
         resultsView.isHidden = false
+        guessField.text = ""
+    }
+    
+    private func success() {
+        let alertTitle = "Success!"
+        let body = "You've cracked this target, great job."
+        let alert = ResultsViewController(title: alertTitle, body: body)
+        self.present(alert, animated: true, completion: nil)
+        nextButton.isHidden = false
+        quitButton.isHidden = false
+    }
+    
+    private func failure() {
+        let alertTitle = "Failed!"
+        let body = "You couldn't crack this target. You are now being tracked by the authorities. I suggest you run, now."
+        let alert = ResultsViewController(title: alertTitle, body: body) { [unowned self] in
+            self.delegate?.gameOver()
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func error() {
+        let alertTitle = "Error"
+        let body = "Could not find any target (network error). Try again later."
+        let alert = ResultsViewController(title: alertTitle, body: body) { [unowned self] in
+            self.delegate?.gameOver()
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func reset() {
+        nextButton.isHidden = true
+        quitButton.isHidden = true
+        resultsView.isHidden = true
+        guessField.text = ""
+        guesses = []
+        guessDict = [:]
+        victory = false
+        triesTableViewHeight.constant = 42
+        triesTableView.reloadData()
+        if let newCombo = delegate?.requestDigits() {
+            currentCombination = newCombo
+            print(currentCombination)
+        } else {
+            error()
+        }
     }
 }
 

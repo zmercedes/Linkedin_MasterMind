96eb6aa67644caf0167492268a97343108929dd9
zmercedes <zmercedes89@gmail.com>
2020-02-05 22:11:48 -0500

    feat: add UIView setStyle extension

---
+++ MasterMind/Extensions/UIView.swift
@@ -0,0 +1,17 @@
+//
+//  UIView.swift
+//  MasterMind
+//
+//  Created by Zoilo Mercedes on 2/5/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func setStyle() {
+        layer.cornerRadius = 6
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
